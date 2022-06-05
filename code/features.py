7766cd17c02d411d231590037a7c60c542cff52d
hz <shinianrumu@613.com>
2022-06-05 22:51:14 +0800

    update

--- code/features.py
+++ code/features.py
@@ -1,20 +1,24 @@
 import pefile
 import os
-from capstone import *
+import capstone
 
 api_mapping = {}
-with open('resources/api.txt') as f:
+with open('../resources/api.txt') as f:
     for idx, api_name in enumerate(f.readlines()):
-        api_mapping[api_name.strip()] = idx + 1
-        api_mapping[str(idx + 1)] = api_name.strip()
+        api_name = api_name.strip()
+        api_mapping[api_name] = idx + 1  # index 0 reserves for padding
+        api_mapping[str(idx + 1)] = api_name
 
 
-def get_api_set_single(file_path, to_id=True):
+def get_api_set_single(file_path, to_id: 'list api with name or id' = True):
     try:
         pe = pefile.PE(file_path)
-    except Exception as e:
-        e = TypeError("unsupported type or damaged file")
-        e.s
+    except Exception:
+        raise TypeError("unsupported type or damaged file")
+
+    if pe.DIRECTORY_ENTRY_IMPORT is None:
+        raise ValueError("no imports")
+
     api_set = []
     for dll in pe.DIRECTORY_ENTRY_IMPORT:
         for imp in dll.imports:
@@ -25,7 +29,7 @@ def get_api_set_single(file_path, to_id=True):
                         api_set.append(api_mapping[name])
                     else:
                         api_set.append(name)
-            except Exception as e:
+            except Exception:
                 continue
     return api_set
 
@@ -37,33 +41,34 @@ def get_api_set_batch(directory_path, to_id=True):
     for index, file in enumerate(file_ls):
         file_path = os.path.join(directory_path, file)
         try:
-            api_ls = get_api_set_single(file_path)
-            if len(api_ls) > 0:
-                api_sets.append(api_ls)
-                print(index)
-        except Exception as e:
-            print(file)
-
+            api_ls = get_api_set_single(file_path, to_id)
+            api_sets.append(api_ls)
+        except TypeError:
+            continue
+        except ValueError:
+            api_sets.append([0])
     return api_sets
 
 
-def get_api_seq_single(file_path, arch=CS_ARCH_X86, mode=CS_MODE_32):
+def get_api_seq_single(file_path, arch=capstone.CS_ARCH_X86, mode=capstone.CS_MODE_32, to_id=True):
     pe = pefile.PE(file_path)
     eop = pe.OPTIONAL_HEADER.AddressOfEntryPoint
     code_section = pe.get_section_by_rva(eop)
     code_dump = code_section.get_data()
     code_addr = pe.OPTIONAL_HEADER.ImageBase + code_section.VirtualAddress
-    # api mapping
-    api_mapping = {}
+    api_address = {}
+    cnt = 0
     for dll in pe.DIRECTORY_ENTRY_IMPORT:
         for imp in dll.imports:
             try:
-                api_mapping[hex(imp.address)] = str(imp.name, encoding='utf-8')
-                api_mapping[str(imp.name, encoding='utf-8')] = hex(imp.address)
+                addr, name = hex(imp.address), str(imp.name, encoding='utf-8')
+                api_address[addr], api_address[name] = name, addr
+                cnt += 1
             except Exception as e:
                 pass
+    print(cnt)
 
-    md = Cs(arch, mode)
+    md = capstone.Cs(arch, mode)
     md.detail = True
     md.skipdata = True
     md.skipdata_setup = ("db", None, None)
@@ -73,8 +78,7 @@ def get_api_seq_single(file_path, arch=CS_ARCH_X86, mode=CS_MODE_32):
     for i in md.disasm(code_dump, code_section.VirtualAddress):
         # print("0x%x:\t%s\t%s" % (i.address, i.mnemonic, i.op_str))
 
-        if i.mnemonic in ('call', 'jmp'):
-
+        if i.mnemonic == 'call':
             split = i.op_str.split(' ')
             if len(split) == 1:
                 address = split[0]
@@ -82,12 +86,10 @@ def get_api_seq_single(file_path, arch=CS_ARCH_X86, mode=CS_MODE_32):
                 address = split[-1][1:-1]
             else:
                 address = ''
-            if address in api_mapping:
-                if api_mapping[address] in api_name_to_id:
-                    api_call_seq.append(api_name_to_id[api_mapping[address]])
-
-    if len(api_call_seq) == 0:
-        api_call_seq.append('0')
+            if address in api_address:
+                name = api_address[address]
+                if name in api_mapping:
+                    api_call_seq.append(api_mapping[name] if to_id else name)
 
     return api_call_seq
 
@@ -98,7 +100,7 @@ def get_api_seq_batch(directory_path):
     api_call_seqs = []
     for index, file in enumerate(file_ls):
         try:
-            api_call_seq = get_api_seq_single(os.path.join(directory_path, file), CS_ARCH_X86, CS_MODE_32)
+            api_call_seq = get_api_seq_single(os.path.join(directory_path, file))
             if len(api_call_seq) > 0:
                 api_call_seqs.append(api_call_seq)
                 print(index)
@@ -117,8 +119,11 @@ def write_feature(lists, directory_path, file_name, append=False):
 
 
 if __name__ == '__main__':
+    # for test
+    demo_file_path = r'D:\actProject\files\unpack\malicious\test' \
+                r'\4810283ba180366087af09329069f8767ba2a4e57fadfb31bab8827d4f90c6cd '
     path = r'D:\actProject\files\unpack\malicious\test'
-    output_path = r'C:\Users\shini\Desktop\api_sequence\unpack\test\malicious.txt'
 
-    get_api_set_batch(directory_path=path)
-
+    # get_disassemble(demo_file_path)
+    single = get_api_seq_single(demo_file_path, to_id=False)
+    print('\n'.join(single))
